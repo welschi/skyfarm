da510d6206f7aac5481e1135e1eb08ca22cca1b1
nwelsch <nicolas@familie-welsch.de>
2022-07-21 19:21:42 +0200

    level.py --> Code etwas angepasst & auf meine Bedürfnisse verschlankt

--- main/level.py
+++ main/level.py
@@ -1,5 +1,11 @@
+########################################################################################################################
+## import alle Packages
+
 import RPi.GPIO as GPIO
 import time
+
+########################################################################################################################
+## Variablen festlegen
 
 GPIO.setmode(GPIO.BCM)
 
@@ -8,6 +14,9 @@ ECHO = 15
 
 GPIO.setup(TRIG, GPIO.OUT)
 GPIO.setup(ECHO, GPIO.IN)
+
+########################################################################################################################
+## Eigentlicher Auslese/Anzeigecode
 
 GPIO.output(TRIG, False)
 print("Starting.....")
@@ -18,13 +27,16 @@ while True:
     time.sleep(0.00001)
     GPIO.output(TRIG, False)
 
+    # Start der Abstandsmessung
     while GPIO.input(ECHO) == 0:
         pulse_start = time.time()
 
     while GPIO.input(ECHO) == 1:
         pulse_stop = time.time()
 
+    # Errechnen der gebrauchten Zeit
     pulse_time = pulse_stop - pulse_start
 
+    # Errechnen des Abstandes
     distance = pulse_time * 17150
     print(str(round(distance, 2)) + "mm")
