e1407641918176ec494e4742bee18f7c0e20e7a5
nwelsch <nicolas@familie-welsch.de>
2022-09-22 10:11:31 +0200

    Aufgeräumt

--- main/terminal.py
+++ main/terminal.py
@@ -3,29 +3,40 @@ from humidity import aktuelleHumidity, dhtDevice
 from level import aktuellerWasserstand
 import time
 
+import os
+
 zaehler = 0
 anzahl = 1000000
 pause = 1
+
+
+def printText(messdaten_level, messdaten_temperatur, messdaten_humidity):
+    os.system("clear")
+    text = f"""
+    Der aktuelle Wasserstand beträgt: {messdaten_level} cm bis zur Wasseroberfläche.
+    Die aktuelle Temperatur beträgt: {messdaten_temperatur} °C.
+    Die aktuelle Luftfeuchtigkeit beträgt: {messdaten_humidity} %.
+    """
+    print(text)
+
 
 while zaehler <= anzahl:
     try:
         messdaten_humidity = aktuelleHumidity()
         messdaten_temperatur = aktuelleTemperatur()
         messdaten_level = aktuellerWasserstand()
-
-        text = "Der aktuelle Wasserstand beträgt: " + str(messdaten_level) + "cm bis zur Wasseroberfläche.\n" + "Die aktuelle Temperatur beträgt: " + str(messdaten_temperatur) + "°C.\n" + "DIe aktuelle Luftfeuchtigkeit beträgt: " + str(aktuelleHumidity) + "%."
-
-        print(text, end="\r")
+        printText(messdaten_level, messdaten_temperatur, messdaten_humidity)
 
         time.sleep(pause)
         zaehler = zaehler + 1
 
     except RuntimeError as error:
-        time.sleep(1.0)
+        print(error)
+        time.sleep(pause)
         continue
 
     except Exception as error:
         dhtDevice.exit()
         raise error
 
-    time.sleep(1.0)
+    time.sleep(pause)
